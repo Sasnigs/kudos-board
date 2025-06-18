40d263fc3d5c59aee0c3573faa8c2039e8c9a0a4
Osasenaga Oluwagbemiga Obaseki <seki@meta.com>
2025-06-17 20:47:46 -0700

    modified routes in server to handle erros

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,6 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
-
 // GET ALL BOARDS
 app.get("/get-boards", async (req, res) => {
   const { query } = req.query;
@@ -16,7 +15,7 @@ app.get("/get-boards", async (req, res) => {
     let whereClause = {};
     if (query) {
       whereClause = {
-        title: { contains: query, mode: 'insensitive' },
+        title: { contains: query, mode: "insensitive" },
       };
     }
     const boards = await prisma.boards.findMany({
@@ -25,24 +24,23 @@ app.get("/get-boards", async (req, res) => {
     });
     res.status(200).json(boards);
   } catch (error) {
-    res.status(500).json({ error: "Failed to get boards" });
+    res.status(500).json({ message: "Failed to get boards" });
   }
 });
 //  GET A SPECIFIC BOARD
 app.get("/get-boards/:id", async (req, res) => {
-  const {id} = req.params
-  try{
+  const { id } = req.params;
+  try {
     const board = await prisma.boards.findUnique({
-        where: {id},
-        include: { cards :true},
-    })
-    res.status(201).json(board)
+      where: { id },
+      include: { cards: true },
+    });
+    res.status(201).json(board);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to get boards" });
   }
-  catch (error) {
-    res.status(500).json({ error: "Failed to get boards" });
-}
 });
-// CREATE A NEW BOARD 
+// CREATE A NEW BOARD
 app.post("/create-board", async (req, res) => {
   const { title, category, author } = req.body;
   if (!title || !category) {
@@ -58,24 +56,24 @@ app.post("/create-board", async (req, res) => {
     });
     res.status(201).json(newBoard);
   } catch (error) {
-    res.status(500).json({ error: "Failed to get boards" });
+    res.status(500).json({ message: "Failed to get boards" });
   }
 });
 
-
 // DELETE A SPECIFIC BOARD
-app.delete('/delete-boards/:id',  async (req, res) => {
-    const {id} = req.params
-    try{
-        await prisma.boards.delete({
-        where: { id }
-    })
-    res.status(200).json({message: "Board deleted sucessfully"})
-    }
-    catch(error){
-    res.status(500).json({ error: "failed to delete boards" });
-    }
-})
+app.delete("/delete-boards/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedBoard = await prisma.boards.delete({
+      where: { id },
+    });
+    res
+      .status(200)
+      .json({ id: deletedBoard.id, message: "Board deleted sucessfully" });
+  } catch (error) {
+    res.status(500).json({ message: "failed to delete boards" });
+  }
+});
 
 // CREATE A CARD IN A SPECIFIC BOARD
 
@@ -90,48 +88,52 @@ app.post("/cards", async (req, res) => {
         title,
         message,
         gif,
-        boardId
+        boardId,
       },
     });
     res.status(201).json(newCard);
   } catch (error) {
-    res.status(500).json({ error: "Failed to create crard" });
+    res.status(500).json({ message: "Failed to create crard" });
   }
 });
 
 // DELETES A SPECIFIC CARD
 
-app.delete('/cards/:id',  async (req, res) => {
-    const {id} = req.params
-    try{
-        await prisma.cards.delete({
-            where: {id}
-        })
-        res.status(200).json({message: "card deleted sucessfully"})
-    }
-     catch(error){
-    res.status(500).json({ error: "failed to delete card" });
-    }
-
-})
+app.delete("/cards/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedCard = await prisma.cards.delete({
+      where: { id },
+    });
+    res
+      .status(200)
+      .json({ id: deletedCard.id, message: "card deleted sucessfully" });
+  } catch (error) {
+    res.status(500).json({ message: "failed to delete card" });
+  }
+});
 //UPDATES UPVOTE COUNT FOR A SPECIFIC CARD
-app.patch('/cards/:id/upvote', async (req, res) => {
-    const {id} =  req.params
-    try{
-        const updateVote = await prisma.cards.update({
-            where: {id},
-            data:{
-                upvotes: {
-                    increment: 1,
-                },
-            },
-        })
-        res.status(201).json({ success: true, data: updateVote })
+app.patch("/cards/:id/upvote", async (req, res) => {
+  const { id } = req.params;
+  // TODO: HANDLE IF ID NOT FOUND/ DOESN'T EXIST
+  try {
+    const card = await prisma.cards.findUnique({ where: { id } });
+    if (!card) {
+      return res.status(404).json({ message: "Card not found" });
     }
-    catch(error){
-    res.status(500).json({ error: "failed to delete card" });
-}
-})
+    const updateVote = await prisma.cards.update({
+      where: { id },
+      data: {
+        upvotes: {
+          increment: 1,
+        },
+      },
+    });
+    res.status(201).json({ success: true, data: updateVote });
+  } catch (error) {
+    res.status(500).json({ message: "failed to upvote" });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
